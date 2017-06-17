e08f92c036deabdcf3f89e3bb29049087793c865
Mateus Craveiro <mccraveiro@gmail.com>
2017-06-17 13:24:24 -0300

    cli: add region and stage options

--- bin/index.js
+++ bin/index.js
@@ -5,6 +5,15 @@ const { init } = require('../index.js')
 
 program
   .version('0.1.0')
+  .option('-r, --region [value]', 'project region')
+  .option('-s, --stage [value]', 'project stage')
   .parse(process.argv)
 
-init()
+const options = {
+  project: {
+    region: program.region,
+    stage: program.stage,
+  },
+}
+
+init(options)
